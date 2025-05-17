f6967c94d7cba380c3515b26146861b28c978986
sdobreff <stoil.dobrev@ns-mediagroup.com>
2025-05-17 06:38:26 +0000

    fadeout

--- classes/lists/class-crons-list.php
+++ classes/lists/class-crons-list.php
@@ -928,10 +928,18 @@ if ( ! class_exists( '\ADVAN\Lists\Crons_List' ) ) {
 							jQuery.get(ajaxurl, data, function(response) {
 								if ( 2 === response['data'] || 0 === response['data'] ) {
 
-									let success = '<?php echo \esc_html__( 'Successfully run', '0-day-analytics' ); ?>';
-									jQuery(that).closest("tr").after('<tr><td style="overflow:hidden;" colspan="'+(jQuery(that).closest("tr").find("td").length+1)+'"><div class="updated" style="background:#fff; color:#000;"> ' + success + '</div></td></tr>');
+									
+										let success = '<?php echo \esc_html__( 'Successfully run', '0-day-analytics' ); ?>';
+										let dynRun = jQuery(that).closest("tr").after('<tr><td style="overflow:hidden;" colspan="'+(jQuery(that).closest("tr").find("td").length+1)+'"><div class="updated" style="background:#fff; color:#000;"> ' + success + '</div></td></tr>');
+										dynRun.next('tr').fadeOut( 5000, function() {
+											dynRun.next('tr').remove();
+										});
+									
 								} else {
-									jQuery(that).closest("tr").after('<tr><td style="overflow:hidden;" colspan="'+(jQuery(that).closest("tr").find("td").length+1)+'"><div class="error" style="background:#fff; color:#000;"> ' + response['data'] + '</div></td></tr>');
+									let dynRun = jQuery(that).closest("tr").after('<tr><td style="overflow:hidden;" colspan="'+(jQuery(that).closest("tr").find("td").length+1)+'"><div class="error" style="background:#fff; color:#000;"> ' + response['data'] + '</div></td></tr>');
+									dynRun.next('tr').fadeOut( 5000, function() {
+										dynRun.next('tr').remove();
+									});
 								}
 							}, 'json');
 
